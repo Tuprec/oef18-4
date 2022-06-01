d3db573ef7c006db63680035319879e4c055c56e
Thibe Provost <thibe.provost@viso-cor-mariae.be>
2022-06-02 00:15:42 +0200

    Add project files.

---
+++ oef18-4/WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="oef18_4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Geef je wachtwoord in (<asp:TextBox ID="TextBox2" runat="server" BackColor="Black" BorderColor="Black" BorderStyle="None" Font-Bold="True" Font-Size="Large" ForeColor="White" Width="55px">1234</asp:TextBox>
+                )</h1>
+
+            <br />
+            Wachtwoord:
+            <asp:TextBox ID="TextBox1" runat="server" TextMode="Password"></asp:TextBox>
+            <asp:Label ID="Label2" runat="server" Text="Niet hetzelfde " Visible="False"></asp:Label>
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="3 Keer fout!!!!!  time out (5 seconden)" Visible="False"></asp:Label>
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="check" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ oef18-4/WebForm1.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Threading;
+
+namespace oef18_4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text != TextBox2.Text)
+            {
+                Label2.Visible = true;
+
+                HttpCookie cookie = Request.Cookies["SurveyCookie"];
+                if (cookie == null)
+                {
+                    // no cookie found, create it
+                    cookie = new HttpCookie("SurveyCookie");
+                    cookie.Values["surveyPage"] = "0";
+
+                }
+                else
+                {
+                    // update the cookie values
+                    int newSurveyPage = int.Parse(cookie.Values["surveyPage"]) + 1;
+                    cookie.Values["surveyPage"] = newSurveyPage.ToString();
+                }
+
+                // update the expiration timestamp
+                cookie.Expires = DateTime.UtcNow.AddDays(30);
+
+                // overwrite the cookie
+                Response.Cookies.Add(cookie);
+
+
+                if (Convert.ToInt32(cookie.Value[0]) == 3)
+                {
+                    Label1.Visible = true;
+                    Button1.Visible = false;
+                    Thread.Sleep(5000);
+                    Label1.Visible = false;
+                    Button1.Visible = true;
+                    cookie.Value = "0";
+                }
+            }
+            else
+            {
+                Label2.Visible=false;
+            }
+        }
+    }
+}
